e382618795ddeb5248f0341accccca1f8310d873
Chris Devereux <devereux.chris@gmail.com>
2013-07-29 22:39:09 +0100

    documentation

--- Source/NSNetService+ReactiveNetService.h
+++ Source/NSNetService+ReactiveNetService.h
@@ -10,9 +10,27 @@
 
 @interface NSNetService (ReactiveNetService)
 
-+ (RACSignal *)rac_resolvedServicesOfType:(NSString *)type inDomain:(NSString *)domainString;
+/**
+ Returns a signal of RACSequences, each containing the last known availible services
+ of type @e type in domain @e domainString.
+ 
+ The subcription continues until the returned signal is cancelled.
+*/
 + (RACSignal *)rac_servicesOfType:(NSString *)type inDomain:(NSString *)domainString;
 
+/**
+ Returns a signal of RACSequences, each containing the last known availible services
+ of type @e type in domain @e domainString.
+ 
+ The signals are resolved before being sent, so the last signal value represents all
+ services with a known address.
+*/
++ (RACSignal *)rac_resolvedServicesOfType:(NSString *)type inDomain:(NSString *)domainString;
+
+/**
+ Resolves the receiver and sends the receiver via the returned signal when the service
+ resolves.
+*/
 - (RACSignal *)rac_resolveWithTimeout:(NSTimeInterval)timeout;
 
 @end
